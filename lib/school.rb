efef915329f5eb1802792a5e942277658eaa2950
Ben Hogan <j.benhogan@gmail.com>
2020-06-23 14:12:55 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
 # code here!
+
+class School
+  
+  def initialize(school_name)
+    @name = school_name
+    @roster = {}
+  end
+  def add_student(name, grade)
+    @roster[grade] << name
+  end
+  def grade(grade_num)
+    @roster[grade_num]
+  end
+
+end
+    
+  
