b4e52902e3d00defb9edce6fb56046dc1e1984b2
Ben Hogan <j.benhogan@gmail.com>
2020-06-23 14:14:42 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,7 +6,10 @@ class School
     @name = school_name
     @roster = {}
   end
+  attr_reader :roster
+  
   def add_student(name, grade)
+    @roster[grade] = []
     @roster[grade] << name
   end
   def grade(grade_num)
